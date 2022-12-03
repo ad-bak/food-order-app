2650a4ada324b6188f0d013f176ec22724f58024
Adnan <adbak.dev@gmail.com>
2022-12-03 17:22:31 +0100

    added header and button

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import mealsImage from "../../assets/mealsimg.jpg";
+import classes from "./Header.module.css";
+import HeaderCardButton from "./HeaderCardButton";
+
+const Header = (props) => {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>Meals</h1>
+        <HeaderCardButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="table full of food" />
+      </div>
+    </>
+  );
+};
+
+export default Header;
