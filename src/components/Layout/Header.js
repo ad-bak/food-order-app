0094c4bf0852e59bc4fca2f0313221c65123771d
Adnan <adbak.dev@gmail.com>
2022-12-04 01:12:12 +0100

    added animation

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,14 +2,14 @@ import React from "react";
 
 import mealsImage from "../../assets/mealsimg.jpg";
 import classes from "./Header.module.css";
-import HeaderCardButton from "./HeaderCardButton";
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = (props) => {
   return (
     <>
       <header className={classes.header}>
         <h1>Meals</h1>
-        <HeaderCardButton onClick={props.onShowCart} />
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="table full of food" />
